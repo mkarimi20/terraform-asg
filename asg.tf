b2d4cfabd8866d8b73f2074a72c1aa1cc9e5e56c
mo <mkarimidevops@gmail.com>
2020-05-04 23:36:36 -0500

    instance type changed

--- asg.tf
+++ asg.tf
@@ -1,12 +1,12 @@
 resource "aws_launch_configuration" "as_launch_conf" {
   name = "web_conf"
   image_id = "${data.aws_ami.image.id}"
-  instance_type = "t2.micro"  
+  instance_type = "t2.mediom"  
   user_data = "${file("wordpress.sh")}"
 }
 
 
-resource "aws.aws_autoscaling_group" "bar" {
+resource "aws.aws_autoscaling_group" "someone" {
     name = "terraform_asg"
     launch_configuration = "${aws_launch_configuration.as_launch_conf.name}"
     availability_zones = [
