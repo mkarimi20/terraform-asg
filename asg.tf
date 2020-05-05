b896dd7c1926c6ce14d99bafb4de7fbdb40fd983
mo <mkarimidevops@gmail.com>
2020-05-04 23:41:56 -0500

    changed

--- asg.tf
+++ asg.tf
@@ -1,21 +1,22 @@
-resource "aws_launch_configuration" "as_launch_conf" {
+resource "aws_launch_configuration" "as_conf" {
   name = "web_conf"
   image_id = "${data.aws_ami.image.id}"
-  instance_type = "t2.mediom"  
+  instance_type = "t2.micro"  
   user_data = "${file("wordpress.sh")}"
 }
 
 
 resource "aws.aws_autoscaling_group" "someone" {
     name = "terraform_asg"
-    launch_configuration = "${aws_launch_configuration.as_launch_conf.name}"
+    launch_configuration = "${aws_launch_configuration.as_conf.name}"
+
     availability_zones = [
       "us-east-1a",
       "us-east-1b",
-      "us-east-1c"
+      "us-east-1c",
     ] 
     min_size = 1
-    max_size = 1
+    max_size = 2
       
 lifecycle {
   create_before_destroy = true
