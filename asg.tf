85d1b81404d33579b72a89ef0057f6d38419e213
mo <mkarimidevops@gmail.com>
2020-05-04 19:01:43 -0500

    aws asg added

---
+++ asg.tf
@@ -0,0 +1,23 @@
+resource "aws_launch_configuration" "as_launch_conf" {
+  name = "web_conf"
+  image_id = "${data.aws.image.id}"
+  instance_type = "t2.micro"  
+}
+
+
+resource "aws.aws_autoscaling_group" "bar" {
+    name = "terraform_asg"
+    launch_configuration = "${aws_launch_configuration.as_launch_conf}"
+    availiblity_zone = [
+      "us-east-1a",
+      "us-east-1b",
+      "us-east-1c"
+    ] 
+    min_size = 1
+    max_size = 1
+      
+lifecycle {
+  create_before_destroy = true
+}
+  
+}
