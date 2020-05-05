82aee24b8138afe8599d6b445fe76857ce9cf080
mo <mkarimidevops@gmail.com>
2020-05-05 00:40:50 -0500

    cg

--- asg_module/asg.tf
+++ asg_module/asg.tf
@@ -6,10 +6,10 @@ resource "aws_launch_configuration" "as_conf" {
 
   provisioner "remote-exec" {
     connection {
-      host        = "${self.public_ip}"
+      host = "${self.public_ip}"
       type        = "ssh"
-      user        = "centos" 
-    private_key = "${file(~/.ssh/id_rsa)}"
+      user        = "centos"
+      private_key = "${file(~/.ssh/id_rsa)}"
 }
 inline = [
   "sudo yum install -y epel-release",
